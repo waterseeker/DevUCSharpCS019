6675ae3cbf4139da4d3bf45fcf957956a6b4816e
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-25 07:25:07 -0500

    added formatting for salary as currency

--- DevUCSharpCS-ASP_019/Default.aspx.cs
+++ DevUCSharpCS-ASP_019/Default.aspx.cs
@@ -18,6 +18,7 @@ namespace DevUCSharpCS_ASP_019
         {
             int socialSecurityNumber = int.Parse(socialSecurityTextBox.Text);
             long phoneNumber = long.Parse(phoneTextBox.Text);
+            double salary = double.Parse(salaryTextBox.Text);
 
             string result = string.Format("Thank you, {0}, for your business. <br />" +
                 "Your Social Security Number is: {1:000-00-0000}. " +
@@ -25,8 +26,14 @@ namespace DevUCSharpCS_ASP_019
                 "but it's good practice for formatting a return string." +
                 "<br />Your phone number is {2: (000) 000-0000} and " +
                 "is being stored as a long because of limits on ints." +
-                "<br />The date you selected from the calendar is: {3:dddd -- d-MMM-yyyy}",
-                nameTextBox.Text, socialSecurityNumber, phoneNumber, loanDateCalendar.SelectedDate);
+                "<br />The date you selected from the calendar is: {3:dddd -- d-MMM-yyyy}" +
+                "<br />You said your salary is: {4:C}.",
+                nameTextBox.Text, 
+                socialSecurityNumber, 
+                phoneNumber, 
+                loanDateCalendar.SelectedDate, 
+                salary);
+
             resultLabel.Text = result;
         }
     }
